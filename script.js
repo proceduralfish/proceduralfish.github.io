df1214a5d7aaa392a13a0097746db1a9dca81ad2
Alonso Segura <88682643+A01747872@users.noreply.github.com>
2023-09-28 22:05:15 -0600

    v1 show more mobile

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ window.addEventListener("click", function (event) {
     }
 });
 
-// // Event listener to close the modal on mobile by tapping outside on mobile
-// window.addEventListener("touchstart", function(event) {
-//     if (event.target === modal) {
-//         closeModal();
-//     }
-// });
+// Event listener to close the modal on mobile by tapping outside on mobile
+window.addEventListener("touchstart", function(event) {
+    if (window.currentModal && event.target === window.currentModal) {
+        closeModal();
+    }
+});
